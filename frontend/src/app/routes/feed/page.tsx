ff302ed0256db2d10eeaeb4f38346fd1b3cfba6c
Joaquin <joaqobianchi13@gmail.com>
2023-11-30 02:00:55 -0300

    Agregada card de publicaciones para el feed

--- frontend/src/app/routes/feed/page.tsx
+++ frontend/src/app/routes/feed/page.tsx
@@ -1,19 +1,21 @@
-import Sidebar from '@/components/Sidebar/Sidebar'
-import React from 'react'
+import FeedCard from "@/components/Cards/FeedCard"
+import Sidebar from "@/components/Sidebar/Sidebar"
+import React from "react"
 
-const pageFeed
-    = () => {
-        return (
-            <section className='flex flex-row'>
-                <Sidebar />
-                <article className='flex flex-col w-full gap-10'>
-                    aca va lo tuyo joa las publis
-                </article>
-                <aside className='flex flex-col p-5 gap-10'>
-                    y aca los sugeridos
-                </aside>
-            </section>
-        )
-    }
+const pageFeed = () => {
+  return (
+    <section className="flex flex-row">
+      <Sidebar />
+      <article className="flex flex-col w-full gap-10">
+        {/* aca va lo tuyo joa las publis */}
+        <FeedCard></FeedCard>
+        <FeedCard></FeedCard>
+      </article>
+      <aside className="flex flex-col p-5 gap-10">
+        {/* y aca los sugeridos */}
+      </aside>
+    </section>
+  )
+}
 
 export default pageFeed
---
+++ frontend/src/components/Cards/FeedCard.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+function FeedCard() {
+  return (
+    <div className="flex flex-col border p-6 border-black roun rounded-3xl w-[678px] h-[500px]">
+      <div className="flex items-center gap-4">
+        <img src="" alt="" className="bg-black w-12 h-12 rounded-full" />
+        <h3>Nombre publicacion</h3>
+      </div>
+      <p className="mt-5 mb-5">
+        {" "}
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed tempore
+        officiis odit explicabo in minus necessitatibus, sit quas corrupti.
+        Dicta repellendus autem ratione ea et animi odit molestiae? Perferendis,
+        culpa?
+      </p>
+      <div className="bg-gray-300 w-full h-full rounded-xl "> </div>
+    </div>
+  )
+}
+
+export default FeedCard
