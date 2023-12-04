3ff498dbb2289d6438c31f3b9fba6d75771faf6c
Favio Fernandez <favioo.wow@gmail.com>
2023-12-03 22:13:04 -0300

    AlbumControllerStandaloneTest tested

--- backend/src/main/java/com/nocountry/s12/Jwt/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/nocountry/s12/Jwt/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.nocountry.s12.Jwt;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -72,7 +73,4 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         return null;
     }
 
-
-
-    
 }
