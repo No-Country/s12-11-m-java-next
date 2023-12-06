76a96aeda336c368b6a496421311460fed9f233f
Nahu258 <milani.kalitko.3012@gmail.com>
2023-12-06 11:04:23 -0300

    fix: image at login and register layout

--- frontend/src/app/auth/layout.tsx
+++ frontend/src/app/auth/layout.tsx
@@ -3,9 +3,9 @@ import bgImage from "../../assets/img/background.jpg"
 
 const authLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='grid grid-cols-2 h-screen'>
+    <div className='md:grid md:grid-cols-2 h-screen'>
       {children}
-      <div className='flex relative'>
+      <div className='md:flex fixed h-screen left-2/4 w-full hidden'>
         <Image src={bgImage} alt='Background image' />
       </div>
     </div>
