0801155714b223efd5d15d07d7043abb7212d7a4
Joaquin <joaqobianchi13@gmail.com>
2023-12-06 04:58:33 -0300

    feat:  adding Config profile page

--- frontend/src/app/auth/layout.tsx
+++ frontend/src/app/auth/layout.tsx
@@ -1,16 +1,17 @@
 import Link from "next/link"
 
-const authLayout = ({
-    children,
-}: {
-    children: React.ReactNode
-}) => {
-    return (
-        <section className="bg-[#DEDEDE] pt-2">
-            <Link href='/' className='ml-2 w-14 h-14 rounded-full bg-black text-white flex justify-center items-center'>logo</Link>
-            {children}
-        </section>
-    )
+const authLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <section className="bg-negro pt-2">
+      <Link
+        href="/"
+        className="ml-2 w-14 h-14 rounded-full bg-black text-white flex justify-center items-center"
+      >
+        logo
+      </Link>
+      {children}
+    </section>
+  )
 }
 
 export default authLayout
