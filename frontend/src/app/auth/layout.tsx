f0501fb33210ce3cad3f0366c4679fbf8ccde0a1
Nahu258 <milani.kalitko.3012@gmail.com>
2023-12-06 09:49:03 -0300

    feat: add login and register page

--- frontend/src/app/auth/layout.tsx
+++ frontend/src/app/auth/layout.tsx
@@ -1,11 +1,15 @@
-const authLayout = ({
-    children,
-}: {
-    children: React.ReactNode
-}) => {
-    return (
-        children
-    )
+import Image from 'next/image'
+import bgImage from "../../assets/img/background.jpg"
+
+const authLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='grid grid-cols-2 h-screen'>
+      {children}
+      <div className='flex relative'>
+        <Image src={bgImage} alt='Background image' />
+      </div>
+    </div>
+  )
 }
 
 export default authLayout
--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -1,13 +1,13 @@
-import Link from "next/link";
+import Link from 'next/link'
 
-export default function SingUpPage () {
+const LoginPage = () => {
     return (
         <div className='bg-[#151515] text-white flex flex-col p-8'>
             <Link href='/home'>Volver</Link>
             <div className='flex flex-col h-full justify-center items-center'>
                 <div className='mb-9'>
                     <h1 className='text-2xl font-bold mb-2'>Bienvenido!</h1>
-                    <p>Crea una cuenta para comenzar</p>
+                    <p>Inicia sesion para una experiencia completa</p>
                 </div>
                 <form className='flex flex-col gap-6 mb-5 w-2/4'>
                     <div className='flex flex-col gap-2'>
@@ -18,14 +18,12 @@ export default function SingUpPage () {
                         <label>Contraseña</label>
                         <input className='p-2 bg-[#151515] border-2 rounded-xl border-gray-700' type="password" placeholder='Contraseña' />
                     </div>
-                    <div className='flex flex-col gap-2'>
-                        <label>Repetir contraseñaa</label>
-                        <input className='p-2 bg-[#151515] border-2 rounded-xl border-gray-700' type="password" placeholder='Contraseña' />
-                    </div>
-                    <button className='bg-[#DCC6E0] text-black rounded-full p-2'>Registrarme</button>
+                    <button className='bg-[#DCC6E0] text-black rounded-full p-2'>Iniciar sesión</button>
                 </form>
-                <Link href='/auth/login'>Iniciar sesión</Link>
+                <Link href='/auth/register'>Registrarme</Link>
             </div>
         </div>
     )
 }
+
+export default LoginPage
