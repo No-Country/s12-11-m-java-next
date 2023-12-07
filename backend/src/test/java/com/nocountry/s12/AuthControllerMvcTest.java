4b7b3bec30c5cc4b50fe5f07e09c17fad9c39b74
Favio Fernandez <favioo.wow@gmail.com>
2023-12-07 16:18:05 -0300

    mend

--- backend/src/test/java/com/nocountry/s12/AuthControllerMvcTest.java
+++ backend/src/test/java/com/nocountry/s12/AuthControllerMvcTest.java
@@ -1,18 +1,22 @@
 package com.nocountry.s12;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nocountry.s12.Dto.Request.LoginDto;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -22,9 +26,23 @@ public class AuthControllerMvcTest {
   @Autowired
   private MockMvc mockMvc;
 
-  @Test
-  void name() {
-    mockMvc.perform(post("/auth/login").contentType(MediaType.APPLICATION_JSON));
+  private JacksonTester<LoginDto> loginDtoJacksonTester;
 
+  @BeforeEach
+  void setUp() {
+    JacksonTester.initFields(this, new ObjectMapper());
+  }
+
+  @Test
+  void name() throws Exception {
+    LoginDto loginDto = new LoginDto("faviofz1@gmail.com","Faviofz8+");
+
+    MockHttpServletResponse response = mockMvc.perform(
+            post("/auth/login").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)
+                .content(loginDtoJacksonTester.write(loginDto).getJson())).andReturn()
+        .getResponse();
+
+    assertEquals(HttpStatus.OK.value(), response.getStatus());
+    assertFalse(response.getContentAsString().isEmpty());
   }
 }
