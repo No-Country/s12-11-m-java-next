48e9cf1f3945989772b7ad8ed9660b1b6097dc56
Nahu258 <milani.kalitko.3012@gmail.com>
2023-12-07 17:47:57 -0300

    fix: login bg and add arrow icon

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -1,13 +1,17 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import { FaArrowLeft } from "react-icons/fa";
 
 const LoginPage = () => {
     return (
         <div className=' text-white flex flex-col p-8'>
-            <Link href='/home' className='p-8'>Volver</Link>
+            <div className='flex items-center gap-4 pl-3'>
+                <FaArrowLeft />
+                <Link href='/home'>Volver</Link>
+            </div>
             <div className='flex flex-col h-full justify-center items-center'>
                 <div className='mb-9'>
-                    <h1 className='text-2xl font-bold mb-2'>Bienvenido!</h1>
-                    <p>Inicia sesion para una experiencia completa</p>
+                    <h1 className='text-5xl font-bold mb-2'>Bienvenido!</h1>
+                    <p className='font-bold'>Inicia sesion para una experiencia completa</p>
                 </div>
                 <form className='flex flex-col gap-6 my-9 w-[60%]'>
                     <div className='flex flex-col gap-2'>
--- frontend/src/app/auth/register/page.tsx
+++ frontend/src/app/auth/register/page.tsx
@@ -1,13 +1,17 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import { FaArrowLeft } from "react-icons/fa";
 
 export default function SingUpPage () {
     return (
         <div className='text-white flex flex-col p-8'>
-            <Link href='/home'>Volver</Link>
+            <div className='flex items-center gap-4 pl-3'>
+                <FaArrowLeft />
+                <Link href='/home'>Volver</Link>
+            </div>
             <div className='flex flex-col h-full justify-center items-center'>
                 <div className='mb-9'>
-                    <h1 className='text-2xl font-bold mb-2'>Bienvenido!</h1>
-                    <p>Crea una cuenta para comenzar</p>
+                    <h1 className='text-5xl font-bold mb-2'>Bienvenido!</h1>
+                    <p className='font-bold'>Crea una cuenta para comenzar</p>
                 </div>
                 <form className='flex flex-col gap-6 my-9 w-[60%]'>
                     <div className='flex flex-col gap-2'>
