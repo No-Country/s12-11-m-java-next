117a417e034ef796a9bd528377e374dd885e13eb
Fernando <fernando-perugachi@hotmail.com>
2023-12-07 08:46:27 -0500

    feat: Add components view discover

---
+++ frontend/src/components/genres/Genres.tsx
@@ -0,0 +1,23 @@
+export const Genres = () => {
+  const genres = [
+    { id: 1, genre: "Rock" },
+    { id: 2, genre: "Pop" },
+    { id: 3, genre: "Electronic" },
+    { id: 4, genre: "Reggae" },
+    { id: 5, genre: "Jazz" },
+    { id: 6, genre: "Rap" },
+  ]
+
+  // interface Genre {
+  //   id: number
+  //   genre: string
+  // }
+
+  console.log(genres)
+
+  return (
+    <section>
+      <div></div>
+    </section>
+  )
+}
