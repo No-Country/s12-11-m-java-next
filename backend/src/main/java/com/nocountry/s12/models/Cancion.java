56366b404f85d666068ba5d5a1300292b41dce9d
leonardofvp <leonardovargasfp@gmail.com>
2023-12-09 18:46:32 -0300

    implementación de carga, descarga y listado de musica

--- backend/src/main/java/com/nocountry/s12/models/Cancion.java
+++ backend/src/main/java/com/nocountry/s12/models/Cancion.java
@@ -10,8 +10,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,20 +28,35 @@ import lombok.NoArgsConstructor;
  */
 @Data
 @Entity
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Table(name="canciones")
 public class Cancion {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String titulo;
-    private String Genero;
+    private String genero;
     private LocalDate fechaSubida;
-    private String imgUrl;
-    private String audioUrl;
+    private String urlCancion;
     
     @ManyToOne
     @JoinColumn(name="id_album")
+    @JsonIgnore
     private Album album;
+    
+    @OneToMany
+    private List<Imagen> imgMusic;
+    
+    
+    public Cancion(String titulo, String genero, String fechaSubida,  Album album, Imagen img, String urlCancion) {
+    	this.titulo =titulo;
+    	this.genero =genero;
+    	this.fechaSubida = LocalDate.parse(fechaSubida); 
+    	this.album=album;
+    	this.imgMusic= new ArrayList<>();
+    	this.imgMusic.add(img);
+    	this.urlCancion=urlCancion;
+    
+    }
 }
