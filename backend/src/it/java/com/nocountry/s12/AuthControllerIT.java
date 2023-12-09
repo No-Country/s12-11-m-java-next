ac0ff3f0b29bddc2b3d7b05247c1eb88c07cd7d9
Favio Fernandez <favioo.wow@gmail.com>
2023-12-09 15:06:34 -0300

    AuthControllerIT

--- backend/src/it/java/com/nocountry/s12/AuthControllerIT.java
+++ backend/src/it/java/com/nocountry/s12/AuthControllerIT.java
@@ -5,12 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nocountry.s12.Dto.Request.LoginDto;
-import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
@@ -21,28 +20,36 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class AuthControllerMvcTest {
+@AutoConfigureJsonTesters
+public class AuthControllerIT {
 
   @Autowired
   private MockMvc mockMvc;
 
+  @Autowired
   private JacksonTester<LoginDto> loginDtoJacksonTester;
+
+  private LoginDto loginDto;
 
   @BeforeEach
   void setUp() {
-    JacksonTester.initFields(this, new ObjectMapper());
+    loginDto = new LoginDto("faviofz1@gmail.com","Faviofz8+");
   }
 
   @Test
-  void name() throws Exception {
-    LoginDto loginDto = new LoginDto("faviofz1@gmail.com","Faviofz8+");
-
+  void canLoginWhenUserExists() throws Exception {
+    //when
     MockHttpServletResponse response = mockMvc.perform(
             post("/auth/login").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)
                 .content(loginDtoJacksonTester.write(loginDto).getJson())).andReturn()
         .getResponse();
-
+    
+    //then
     assertEquals(HttpStatus.OK.value(), response.getStatus());
     assertFalse(response.getContentAsString().isEmpty());
+  }
+
+  @Test
+  void canRegisterAUserWithValidCredentials() {
   }
 }
