ff9ef4ad8b0eadbed5d7ba8da029b1d29383b5ab
Fernando <fernando-perugachi@hotmail.com>
2023-12-12 09:30:00 -0500

    feat: Add components Genres, Music and Song in the folder descubre

--- frontend/src/app/(routes)/descubre/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const pageDescubre = () => {
-    return (
-        <div>pageDescubre</div>
-    )
-}
-
-export default pageDescubre
---
+++ frontend/src/components/descubre/music/Music.tsx
@@ -0,0 +1,11 @@
+import { Song } from "./Song"
+
+export const Music = () => {
+  return (
+    <section className="grid gap-4 py-12">
+      <Song />
+      <Song />
+      <Song />
+    </section>
+  )
+}
