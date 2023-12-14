6b5d035444139ff1f8a7592404bc6beb58f48e6e
Fernando <fernando-perugachi@hotmail.com>
2023-12-13 21:31:44 -0500

    fix: Line moved for error

--- frontend/src/app/(routes)/perfil/layout.tsx
+++ frontend/src/app/(routes)/perfil/layout.tsx
@@ -1,24 +1,19 @@
-import { RedirectNoLogin } from '@/components/RedirectNoLogin/RedirectNoLogin'
-import AccountNavBar from '@/components/Account/AccountNavBar'
-import React from 'react'
-import Account from '@/components/Account/Account'
+import { RedirectNoLogin } from "@/components/RedirectNoLogin/RedirectNoLogin"
+import AccountNavBar from "@/components/Account/AccountNavBar"
+import React from "react"
+import Account from "@/components/Account/Account"
 
-const pagePerfil = ({
-    children,
-}: {
-    children: React.ReactNode
-}) => {
-    return (
-        <section className='flex flex-col'>
-            <RedirectNoLogin />
-            <Account />
-            <article className='flex flex-col w-full gap-5 px-5 bg-darkViolet text-white min-h-[42vh] h-auto py-5'>
-                <AccountNavBar />
-                {children}
-            </article>
-        </section>
-
-    )
+const pagePerfil = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <section className="flex flex-col">
+      <RedirectNoLogin />
+      <Account />
+      <article className="flex flex-col w-full gap-5 px-5 bg-darkViolet text-white min-h-[42vh] h-auto py-5">
+        <AccountNavBar />
+        {children}
+      </article>
+    </section>
+  )
 }
 
 export default pagePerfil
