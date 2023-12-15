5ed22b2a908db60b92c109a464f3c4bd83f6c520
Fernando <fernando-perugachi@hotmail.com>
2023-12-15 17:36:01 -0500

    test: Add new endpoint in files .env and register

--- frontend/src/utils/registerRequest/registerPost.tsx
+++ frontend/src/utils/registerRequest/registerPost.tsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 const registerPost = async (userData: any, router: any) => {
   await axios
-    .post(`http://3.145.112.182:8080/auth/registro`, userData)
+    .post(`http://3.144.7.16:8080/auth/registro`, userData)
     .then(function (response: any) {
       console.log("se registro pa")
       router.push("/auth/login")
