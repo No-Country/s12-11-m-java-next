dfd39469e14aace65597a44ddbe14804b02060b7
leonardofvp <leonardovargasfp@gmail.com>
2023-12-19 18:58:15 -0300

    modificacion de app conf para permitir todos los origenes

--- backend/src/main/java/com/nocountry/s12/Config/AppConfig.java
+++ backend/src/main/java/com/nocountry/s12/Config/AppConfig.java
@@ -56,9 +56,9 @@ public class AppConfig {
             @Override
             public void addCorsMappings(@NotNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:3000" )
-                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE","OPTIONS")
-                        .allowCredentials(true);
+                        .allowedOrigins("*" )
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE","OPTIONS");
+                       // .allowCredentials(true);
             }
         };
     }
